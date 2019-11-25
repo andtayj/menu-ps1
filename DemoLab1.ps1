ff3e815a49f9cb2f47905816fef9a4d0ab6430bf
Andreasen <andtayj@dunwoody.edu>
2019-11-25 14:22:40 -0600

    comment

---
+++ DemoLab1.ps1
@@ -0,0 +1,16 @@
+﻿Clear-Host
+
+$First = "Bob"
+$Last = "Smith"
+Write-Output "Test 1: First name is $First and last name is $Last"
+Read-Host "Press Enter to continue. . ."
+
+$City = Read-Host "What is the city?"
+$State = Read-Host "What is the state?"
+Write-Output "Test 2: The city of $City in the state of $State"
+Read-Host "Press Enter to continue. . ."
+
+Get-Date
+$TheDate = Get-Date
+Write-Output "Test 3: The date is $TheDate"
+Read-Host "Press Enter to continue. . ."
